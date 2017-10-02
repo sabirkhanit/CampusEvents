8c74c4f2a51d8928ce8f40c226893b154ecc9eef
sabir2005 <mskhan98@gmail.com>
2017-10-02 17:00:43 +0530

    data source exclusion
    data source exclusion

--- campusevents-api/src/main/java/api/campus/events/CampusEventsAPI.java
+++ campusevents-api/src/main/java/api/campus/events/CampusEventsAPI.java
@@ -1,13 +1,15 @@
 package api.campus.events;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class CampusEventsAPI {
 
 	public static void main(String[] args) {
 		
-		
+		SpringApplication.run(CampusEventsAPI.class, args);
 	}
 
 }
---
+++ campusevents-api/src/main/java/api/campus/events/configuration/CampusEventsConfiguration.java
@@ -0,0 +1,5 @@
+package api.campus.events.configuration;
+
+public class CampusEventsConfiguration {
+
+}

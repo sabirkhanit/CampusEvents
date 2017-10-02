87d7b6a971bf128c8c9ee378ac3b1e9399402d7e
sabir2005 <mskhan98@gmail.com>
2017-10-02 16:52:33 +0530

    Project Initiation
    Project Initiation

---
+++ campusevents-api/src/main/java/api/campus/events/CampusEventsAPI.java
@@ -0,0 +1,13 @@
+package api.campus.events;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusEventsAPI {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
---
+++ campusevents-api/src/main/java/api/campus/events/domain/Event.java
@@ -0,0 +1,5 @@
+package api.campus.events.domain;
+
+public class Event {
+
+}
